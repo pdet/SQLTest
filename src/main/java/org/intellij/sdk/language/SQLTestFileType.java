88b3bee2a1cda936254bce5cb39700731010f8aa
Pedro Holanda <pedroholanda@gmail.com>
2024-04-22 17:22:38 +0200

    Add test_slow to files and new release

--- src/main/java/org/intellij/sdk/language/SQLTestFileType.java
+++ src/main/java/org/intellij/sdk/language/SQLTestFileType.java
@@ -29,7 +29,7 @@ public class SQLTestFileType extends LanguageFileType {
     @NotNull
     @Override
     public String getDefaultExtension() {
-        return "test;testslow;test_slow";
+        return "test";
     }
 
     @Nullable
