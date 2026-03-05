9a77a7d76fee632abb451402e407033a8cb8d44d
Peter Van Holland <PeterVanHolland@users.noreply.github.com>
2026-03-05 14:17:33 +0000

    feat: gutter icons for statement ok/error and query blocks
    - Green checkmark icon for 'statement ok' lines
    - Red X icon for 'statement error' lines
    - Ignored icon for 'statement maybe' lines
    - Play/execute icon for 'query' lines
    - Tooltip shows expected behavior on hover
    
    Part of #13 (Phase 4: Navigation & usability)

---
+++ src/main/java/org/intellij/sdk/language/SQLTestLineMarkerProvider.java
@@ -0,0 +1,71 @@
+package org.intellij.sdk.language;
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.icons.AllIcons;
+import com.intellij.openapi.editor.markup.GutterIconRenderer;
+import com.intellij.psi.PsiElement;
+import org.intellij.sdk.language.psi.TestTypes;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+/**
+ * Adds gutter icons for test directives:
+ * - Green checkmark for 'statement ok'
+ * - Red X for 'statement error'
+ * - Blue arrow for 'query'
+ */
+public class SQLTestLineMarkerProvider implements LineMarkerProvider {
+
+    @Override
+    public @Nullable LineMarkerInfo<?> getLineMarkerInfo(@NotNull PsiElement element) {
+        if (element.getNode().getElementType() != TestTypes.STATEMENT &&
+            element.getNode().getElementType() != TestTypes.QUERY) {
+            return null;
+        }
+
+        // Look at the text after the keyword
+        PsiElement next = element.getNextSibling();
+        StringBuilder rest = new StringBuilder();
+        while (next != null && !next.getText().contains("\n")) {
+            rest.append(next.getText());
+            next = next.getNextSibling();
+        }
+
+        String afterKeyword = rest.toString().trim().toLowerCase();
+
+        Icon icon;
+        String tooltip;
+
+        if (element.getNode().getElementType() == TestTypes.STATEMENT) {
+            if (afterKeyword.startsWith("ok")) {
+                icon = AllIcons.RunConfigurations.TestPassed;
+                tooltip = "Statement expects success";
+            } else if (afterKeyword.startsWith("error")) {
+                icon = AllIcons.RunConfigurations.TestFailed;
+                tooltip = "Statement expects error";
+            } else if (afterKeyword.startsWith("maybe")) {
+                icon = AllIcons.RunConfigurations.TestIgnored;
+                tooltip = "Statement expects maybe";
+            } else {
+                return null;
+            }
+        } else {
+            // Query
+            icon = AllIcons.Actions.Execute;
+            tooltip = "Query block: " + afterKeyword;
+        }
+
+        return new LineMarkerInfo<>(
+                element,
+                element.getTextRange(),
+                icon,
+                e -> tooltip,
+                null,
+                GutterIconRenderer.Alignment.LEFT,
+                () -> tooltip
+        );
+    }
+}
