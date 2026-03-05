588956919ed4b1390b0b9e059d25daf813e55487
Peter Van Holland <PeterVanHolland@users.noreply.github.com>
2026-03-05 12:56:11 +0000

    feat: context-aware highlighting with new directives and result dimming
    - Add directive keywords: require, require-env, mode, halt, skipif,
      onlyif, foreach, endforeach, concurrentloop, concurrentforeach
    - All directives highlighted consistently as metadata (RESERVED)
    - Result section lines (after ----) now use DOC_COMMENT style (dimmed)
      instead of keyword highlighting, clearly distinguishing query output
      from SQL code
    - Added RESULT_LINE token type for result section content
    
    Part of #13 (Phase 3: Context-aware highlighting)

--- src/main/gen/org/intellij/sdk/language/psi/TestTypes.java
+++ src/main/gen/org/intellij/sdk/language/psi/TestTypes.java
@@ -7,14 +7,18 @@ import com.intellij.lang.ASTNode;
 
 public interface TestTypes {
 
-
   IElementType BEGIN = new TestTokenType("begin");
   IElementType COMMA = new TestTokenType(",");
   IElementType COMMENT = new TestTokenType("comment");
+  IElementType CONCURRENTFOREACH = new TestTokenType("concurrentforeach");
+  IElementType CONCURRENTLOOP = new TestTokenType("concurrentloop");
   IElementType DDOT = new TestTokenType(":");
   IElementType DOT = new TestTokenType(".");
+  IElementType ENDFOREACH = new TestTokenType("endforeach");
   IElementType ENDLOOP = new TestTokenType("endloop");
   IElementType EQ = new TestTokenType("=");
+  IElementType FOREACH = new TestTokenType("foreach");
+  IElementType HALT = new TestTokenType("halt");
   IElementType HIGHER = new TestTokenType(">");
   IElementType ID = new TestTokenType("id");
   IElementType LESS = new TestTokenType("<");
@@ -22,7 +26,9 @@ public interface TestTypes {
   IElementType LOOP = new TestTokenType("loop");
   IElementType LP = new TestTokenType("(");
   IElementType MINUS = new TestTokenType("-");
+  IElementType MODE = new TestTokenType("mode");
   IElementType NUMBER = new TestTokenType("number");
+  IElementType ONLYIF = new TestTokenType("onlyif");
   IElementType PHYSICAL_PLAN = new TestTokenType("physical_plan");
   IElementType PLUS = new TestTokenType("+");
   IElementType PRAGMA = new TestTokenType("PRAGMA");
@@ -30,10 +36,14 @@ public interface TestTypes {
   IElementType QUERY_LABEL = new TestTokenType("query_label");
   IElementType QUERY_RETURN_TYPE = new TestTokenType("query_return_type");
   IElementType Q_RESULT = new TestTokenType("----");
+  IElementType REQUIRE = new TestTokenType("require");
+  IElementType REQUIRE_ENV = new TestTokenType("require-env");
   IElementType RESTART = new TestTokenType("restart");
+  IElementType RESULT_LINE = new TestTokenType("result_line");
   IElementType ROLLBACK = new TestTokenType("rollback");
   IElementType RP = new TestTokenType(")");
   IElementType SEMI = new TestTokenType(";");
+  IElementType SKIPIF = new TestTokenType("skipif");
   IElementType SPACE = new TestTokenType(" ");
   IElementType SQL = new TestTokenType("sql");
   IElementType STAR = new TestTokenType("*");
